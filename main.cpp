a7bd1695de606451da392a2442be2a906e61f2ca
alvaro aguirre <44936171+chelaschill@users.noreply.github.com>
2020-03-29 19:01:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
-
 #include <iostream>
+#include "sorts.cpp"
 using namespace std;
 
-int main(int, char*[]){
-    return 1;
+template<class T>
+void print_vector(vector<T> * v){
+    for(auto i : *v){
+        cout<<i<<',';
+    }
 }
+
+int main() {
+    auto *Vector_Prueba = new vector<int>();
+    *Vector_Prueba = {3,8,5,1,6};
+    Sorting V1(Vector_Prueba);
+    V1.Merge_Sort(0, Vector_Prueba->size() - 1);
+    print_vector(Vector_Prueba);
+    return 0;
+}
+
